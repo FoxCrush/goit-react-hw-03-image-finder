c576af8f7f5973bb83a45bb0693b6cd1808e0eb7
FoxCrush <63466872+FoxCrush@users.noreply.github.com>
2022-09-14 16:19:49 +0300

    modal added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,22 @@
+import React, { Component } from 'react';
 import Searchbar from './Searchbar';
+import Modal from './Modal';
+import { Fragment } from 'react';
 
-export const App = () => {
-  return <Searchbar />;
-};
+class App extends Component {
+  state = {
+    showModal: false,
+  };
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({ showModal: !showModal }));
+  };
+  render() {
+    return (
+      <Fragment>
+        <Searchbar />
+        {this.state.showModal && <Modal />}
+      </Fragment>
+    );
+  }
+}
+export default App;
